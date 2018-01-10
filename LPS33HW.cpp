7e11e3a86084882c7e3d0359e80a9b561249f5af
chisl <info@chisl.io>
2018-01-10 22:08:24 +0100

    Initial commit

---
+++ LPS33HW.cpp
@@ -0,0 +1,18 @@
+/*
+ * name:        LPS33HW
+ * description: MEMS pressure sensor: 260-1260 hPa absolute digital output barometer with water-resistant package
+ * manuf:       STMicroelectronics
+ * version:     Version 0.1
+ * url:         http://www.st.com/resource/en/datasheet/lps33hw.pdf
+ * date:        2018-01-02
+ * author       https://chisl.io/
+ * file:        LPS33HW.cpp
+ */
+
+/*                                                                                                       *
+ *                                   THIS FILE IS AUTOMATICALLY CREATED                                  *
+ *                                    D O     N O T     M O D I F Y  !                                   *
+ *                                                                                                       */
+
+#include "LPS33HW.hpp"
+
